8ad9d5181ee43a40c07f1e724da930ccba7faec3
erimozcancivgin <126330142+erimozcancivgin@users.noreply.github.com>
2023-02-27 23:46:32 +0300

    Add files via upload

---
+++ first_project-main/README.md
@@ -0,0 +1,12 @@
+# Calculator
+[visit](https://erimozcancivgin.github.io/first_project/calculator/)
+
+
+
+
+
+
+
+
+
+
---
+++ first_project-main/calculator/assets/js/app.js
@@ -0,0 +1,81 @@
+const buttons = document.querySelectorAll("button")
+const storageInput = document.querySelector(".storaged-number")
+const currentInput = document.querySelector(".current-number")
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        const buttonType = button.dataset.type
+        if (buttonType === "number") {
+            currentInput.value = currentInput.value + button.value
+        }
+        if (buttonType === "operation") {
+            if (storageInput.value.length) {
+                equalOperation(storageInput.value.slice(0, -1), currentInput.value, button.value)
+                currentInput.value = ""
+            } else {
+                storageInput.value = currentInput.value + button.value
+                currentInput.value = ""
+            }
+        }
+
+        if (buttonType === "equal") {
+            equalOperation(storageInput.value.slice(0, -1), currentInput.value, storageInput.value.slice(-1))
+            currentInput.value = ""
+            storageInput.value = storageInput.value.slice(0, -1)
+        }
+
+
+        if (buttonType === "clean") {
+            currentInput.value = ""
+        }
+
+
+        if (buttonType === "clean-all") {
+            currentInput.value = ""
+            storageInput.value = ""
+        }
+
+        if (buttonType === "ON") {
+            if (!currentInput.value.length) {
+                currentInput.value = "0"
+            }
+        }
+
+        if (buttonType === "delete") {
+            if (currentInput.value.length) {
+               (currentInput.value.length -1)
+            }
+           
+
+
+        }
+
+
+    })
+
+
+});
+
+const equalOperation = function (storageNumber, currentNumber, operation) {
+    let equal = ""
+
+    if (operation === "+") {
+        equal = Number(storageNumber) + Number(currentNumber) + "+";
+
+    }
+    else if (operation === "-") {
+        equal = Number(storageNumber) - Number(currentNumber) + "-";
+
+    }
+    else if (operation === "*") {
+        equal = Number(storageNumber) * Number(currentNumber) + "*"
+
+    }
+    else if (operation === "/") {
+        equal = Number(storageNumber) / Number(currentNumber) + "/"
+
+    }
+
+
+    storageInput.value = equal
+}
